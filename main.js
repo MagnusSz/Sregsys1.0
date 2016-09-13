25acb3052dd5ab118346b7dffc771547bb20138c
Asger-Storebjerg <201307896@post.au.dk>
2016-09-13 15:43:55 +0200

    Påbegyndt ractive kode
    Påbegyndt ractive kode

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Velkommen til!")
+
+// Først hiver vi vores boards ud
+var projects = ProjectManager.getProjects();
+// Nu laver vi et data objekt til ractive som består af boards, et activeBoard som vi sætter til at være det første i listen, og editedNote som holder styr på hvilken note der editeres.
+var data = {
+  projects: projects;
+  projectName: "",
+  activeProject: Object.keys(projects)[0],
+  productBacklog: {userStories:[]},
+  sprints: [],
+}
+// Nu instantierer vi ractive. Vi giver den en reference til det element vi gerne vil have tegnet vores brugergrænse flade i som en css selector, reference til vores template, vi antager at vi er i en ny browser så vi kan godt bruge magic mode (http://docs.ractivejs.org/latest/magic-mode) og til sidst giver vi ractive vores data objekt.
+var ractive = new Ractive({
+    el: '#content',
+    template: '#projekt',
+    magic: true,
+    data: data,
+});
