85eeb7ae201661e41edef544a2d4c7c79f3f8a4d
MagnusSz <MagnusSzatkowski@gmail.com>
2016-09-13 15:24:02 +0200

    Oprettet projectManager.js

---
+++ ProjectManager.js
@@ -0,0 +1,29 @@
+/*
+SregSys Version 1.0
+Asger Storebjerg og Magnus Szatkowski, 2016
+*/
+
+//Denne fil bruges til at oprette et projekt
+var projectManager = {};
+
+//Denne funktion gemmer et objekt alle projekter i JSONStorage under nøglen project1
+projectManager.store = function(projects) {
+  jsonStorage.setItem("project1", projects);
+}
+
+//Denne funktion henter alle applikationernes projekter ud fra JSONStorage
+projectManager.getProjects = function() {
+  var projects = (jsonStorage.getItem("project1")) || {default: {projectBacklog: {}, sprints[]}});
+  return projects;
+}
+
+// Denne funktion laver en (sandsynligvis) garanteret unik ID streng
+projectManager.guid = function() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+};
